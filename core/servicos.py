850c23fafde8483a19d2ac14afc77fdaff5744d7
Gustavo Cazzine <cazzinengustavo@gmail.com>
2025-07-26 10:08:58 -0300

    feat, refactor, style: Codigo refatorado, adicionado novas funcionalidades e melhorado esquema de tratamento de erros e mensagens intuitivas

--- core/servicos.py
+++ core/servicos.py
@@ -1,4 +1,41 @@
+#Importnado bibliotecas para formatações e organização
 from datetime import datetime, date, timedelta
+import os
+import time
+
+#Função para solicitar nome do serviço e minutos de duração
+def solicitar_informações():
+
+    limpar_tela()
+
+    while True:
+        try:
+            nome_do_servico = str(input("Nome do serviço: "))
+
+            if nome_do_servico:
+                while True: 
+                    try: 
+                        minutos = int(input("Duração do serviço: (minutos): "))
+
+                        if nome_do_servico and minutos:
+                            return nome_do_servico, minutos
+                        
+                        else:
+                            print("Erro inesperado, tente novamente.")
+                            delay(2)
+                            limpar_tela()
+                            break
+                        
+                    except:
+                        
+                        print("Digite a duração total do serviço, em minutos (1 hora = 60 minutos).")
+            else:
+                limpar_tela()
+                print("Campo em branco, informe o nome do serviço")
+        except:
+            print("Insira o nome do serviço, tente novamente...")
+        
+    
 
 #Função para receber o nome e duração do serviço.
 def adicionar_servico(nome_do_servico, minutos):
@@ -15,6 +52,55 @@ def adicionar_servico(nome_do_servico, minutos):
 
 #Função para listar os serviços cadastrados
 def listar_servicos(lista_de_servicos):
-    for servicos in lista_de_servicos:
-        print(f"{servicos["nome do servico"]} - Duração: {servicos["duracao_servico"]}.")
+    limpar_tela()
+    if not lista_de_servicos:
+        print("\nNenhum serviço cadastrado.")
+    else:
+        for servicos in lista_de_servicos:
+            print(f"\n{(servicos["nome do servico"]).capitalize()} - Duração: {servicos["duracao_servico"]}")
 
+#Função para limpar o terminal
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+#Função para uma pausa no terminal
+def delay(segundos, continuar=False):
+    if continuar:
+        input("\nPressione ENTER para continuar...")
+    else:
+        time.sleep(segundos)
+
+#Função para preparar titulos padronizados
+def titulo(msg):
+    print(f"\n--- {msg.upper()} ---\n")
+
+#Função para listar as opções
+def listar_opcoes():
+        
+    opcoes = ["Adicionar Serviço", "Listar Serviços", "Sair\n"]
+
+    for e, opcao in enumerate(opcoes,start=1):
+        print(f"{e}. {opcao}")
+
+    return opcoes
+
+#Função que trabalha junto ao listar_opcoes() e retorna a escolha do usuario
+def escolher_opcao():
+
+    opcoes = listar_opcoes()
+
+    quantidade_opcoes = len(opcoes)
+
+    while True:
+        try:
+            escolha = int(input("Escolha uma opção: "))
+            if escolha > 0 and escolha <= quantidade_opcoes:
+                return escolha
+            else:
+                limpar_tela()
+                print(f"\nEscolha inválida, escolha uma entre as {quantidade_opcoes} opções\n")
+                listar_opcoes()
+        except:
+            limpar_tela()
+            print("\nDigite o número referente a opção desejada, tente novamente...\n")
+            listar_opcoes()
--- main.py
+++ main.py
@@ -1,31 +1,46 @@
 #Importando as funções e bibliotecas
-from core.servicos import adicionar_servico, listar_servicos
-import os
+from core.servicos import adicionar_servico,listar_servicos,limpar_tela,delay,titulo, escolher_opcao, solicitar_informações
 
 #Iniciando as listas para armazenas os agendamentos
 lista_de_servicos = []
 lista_de_agendamentos = []
 
 while True:
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print("1 - Adicionar Serviço \n2 - Listar Serviços")
+    limpar_tela()
 
-    escolha = int(input("Escolha uma opção: "))
+    titulo("agendaSaaS")
+
+    escolha = escolher_opcao()
 
     match escolha:
+        #Escolha 1 - Adicionar um novo serviço
         case 1:
             #Solicita informações de nome e duração do serviço
-            nome_do_servico = str(input("Nome do serviço: "))
-            minutos = int(input("Duração do serviço: (minutos): "))
-            #Adiciona na lista atraves da função adicionar_servico()
-            lista_de_servicos.append(adicionar_servico(nome_do_servico, minutos))
+            nome_do_servico, minutos = solicitar_informações()
 
+            #Adiciona na lista atraves da função adicionar_servico()
+            try:
+                lista_de_servicos.append(adicionar_servico(nome_do_servico, minutos))
+                print(f"\n{nome_do_servico} adicionado com sucesso!")
+            except:
+                print("Erro ao adicionar serviço, tente novamente")
+            delay(2)
+
+        #Escolha 2 - Listar serviços adicionados
         case 2:
             listar_servicos(lista_de_servicos)
+            delay(1, True)
+            limpar_tela()
 
+
+        #Escolha 3 - Encerrar programa
         case 3:
+            limpar_tela()
             print("Finalizando programa...")
-            
+            delay(1)
+            limpar_tela()
             break
+
+        #Caso escolha inválida, aviso e retorna ao loop
         case _:
             print("Escolha uma opção válida. Tente novamente: \n")
